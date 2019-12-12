771927da44ba5e48aa16c45c48297388790416e9
NoblePrinceii <farhan.begg@students.makeschool.com>
2019-12-12 02:31:27 -0800

    Entry Model

--- entries/models.py
+++ entries/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+from django.contrib.auth.models import User
 # Create your models here.
+class Entry(models.Model):
+    entry_title = models.CharField(max_length=50)
+    entry_text = models.TextField()
+    entry_data = models.DateTimeField(auto_now_add=True)
+    entry_author = models.ForeignKey(User, on_delete=models.CASCADE)
